0c05cfacddf9c38c951e682e9494a0558f900a10
Fazri Anggriawan <fazri.anggriawan@gmail.com>
2021-07-03 15:49:16 +0700

    update host di config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,4 @@
+var host = 'http://182.253.22.221';
 export const config = {
-    api_url: function(url:string){ return 'http://localhost/emr/api/public/'+url}
+    api_url: function(url:string){ return host+'/emr/api_emr/public/'+url}
 }
